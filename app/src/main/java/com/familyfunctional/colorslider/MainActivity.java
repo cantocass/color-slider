b8cd51d5acb74c5871e583442563208e0c4e319f
cantocass <cantocass@users.noreply.github.com>
2015-10-23 20:24:45 -0600

    Add space to toast text

--- app/src/main/java/com/familyfunctional/colorslider/MainActivity.java
+++ app/src/main/java/com/familyfunctional/colorslider/MainActivity.java
@@ -162,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void toastToTheCreators() {
-        Toast.makeText(this, getString(R.string.thank_you) + getString(R.string.colourlovers), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, getString(R.string.thank_you) + " " + getString(R.string.colourlovers), Toast.LENGTH_LONG).show();
     }
     //</editor-fold>
 
